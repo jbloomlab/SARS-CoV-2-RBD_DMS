cada1d6e4ea809bdc145ca84297352ccb557ed11
tylernstarr <tyler.n.starr@gmail.com>
2020-06-16 21:47:35 -0700

    Update for new rhino specs

--- run_Hutch_cluster.bash
+++ run_Hutch_cluster.bash
@@ -3,7 +3,7 @@
 snakemake \
     -j 999 \
     --cluster-config cluster.yaml \
-    --cluster "sbatch -p {cluster.partition} -c {cluster.cpus} --mem={cluster.mem} -t {cluster.time} -J {cluster.name}" \
+    --cluster "sbatch -c {cluster.cpus} --mem={cluster.mem} -t {cluster.time} -J {cluster.name}" \
     --latency-wait 30 \
     --use-envmodules \
     --use-conda \
